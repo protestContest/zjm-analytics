d77acce5559eaf367b33425ae128b8988d5b4a5b
Zack Michener <z@zjm.me>
2019-01-20 14:10:20 -0800

    add hit reference, validity test

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,6 @@
 class Site < ApplicationRecord
   belongs_to :user
+  has_many :hits
 
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :user_id
--- test/models/hit_test.rb
+++ test/models/hit_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class HitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:zack)
+    @site = @user.sites.build(name: "Test Site")
+    @hit = @site.hits.build(
+      hit_type: 'pageview',
+      location: 'http://example.com',
+      language: 'en-US',
+      encoding: 'UTF-8',
+      title: 'Example Page',
+      color_depth: 24,
+      screen_res: '1440x900',
+      viewport: '800x600',
+      tracking_id: 'XXX-TEST',
+      client_id: '1234'
+    )
+  end
+
+  test "should be valid" do
+    assert @hit.valid?
+  end
+
 end
