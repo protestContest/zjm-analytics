f6c3aaca9c7ae68d0c6e0a94b13e8f2507d55d78
Zack Michener <z@zjm.me>
2019-01-20 13:08:25 -0800

    get testing working

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
+  gem 'guard'
+  gem 'guard-minitest'
   gem 'selenium-webdriver'
 end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'asdf@example.com', password: 'password')
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
