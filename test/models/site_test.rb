93bb1c713732cf0b0e9aa0ceee1890dae6c8882b
Zack Michener <z@zjm.me>
2019-01-20 13:33:21 -0800

    add user and site validations

--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class SiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:zack)
+    @site = @user.sites.build(name: "Site Name")
+  end
+
+  test "should be valid" do
+    assert @site.valid?
+  end
+
+  test "name should be present" do
+    @site.name = "   "
+    assert_not @site.valid?
+  end
+
+  test "name should be unique for a user" do
+    dup_site = @site.dup
+    @site.save
+    assert_not dup_site.valid?
+  end
+
+  test "name can be reused for different users" do
+    fred = users(:fred)
+    fred_site = fred.sites.build(name: @site.name)
+    @site.save
+    assert fred_site.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,11 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "password length should be at least 8" do
+    @user.password = "asdfasd"
+    assert_not @user.valid?
+    @user.password = "asdfasdf"
+    assert @user.valid?
+  end
 end
