5b51baf5e62ea6e003e573ff6fd6f87239d88421
Zack Michener <z@zjm.me>
2019-02-12 20:36:16 -0800

    update styles more

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module ZjmAnalytics
 
     config.active_job.queue_adapter = :sidekiq
     config.generators.javascript_engine = :js
+    config.assets.precompile += %w( '.svg' )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
