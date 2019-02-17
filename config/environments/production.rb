8c7ad761c15bf79ecb0edcb582883d99fa97b276
Zack Michener <z@zjm.me>
2019-02-17 14:07:50 -0800

    add default host for mailers in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :sidekiq
   config.active_job.queue_name_prefix = "zjm-analytics_#{Rails.env}"
+
+  config.action_mailer.default_url_options = { host: 'analytics.zjm.me' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
