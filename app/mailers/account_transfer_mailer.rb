57aa79a207f087f37b5026066caf39fe12cd950a
Zack Michener <z@zjm.me>
2019-02-19 00:43:56 -0800

    update account transfer mailer

--- app/mailers/account_transfer_mailer.rb
+++ app/mailers/account_transfer_mailer.rb
@@ -3,6 +3,8 @@ class AccountTransferMailer < ApplicationMailer
     transfer = params[:transfer]
     @account = transfer.account
     @transfer_response_url = account_transfer_url(transfer, response_token: transfer.response_token)
+    @accept_url = accept_account_transfer_url(transfer, response_token: transfer.response_token)
+    @reject_url = reject_account_transfer_url(transfer, response_token: transfer.response_token)
     mail(to: transfer.target_owner, subject: 'Account Transfer Request')
   end
 end
