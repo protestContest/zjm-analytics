3ea43768ff374a22b81283ffe772a74270fcf0cf
Zack Michener <zackm@zillowgroup.com>
2019-02-19 10:39:05 -0800

    make account transfer email mobile-friendly

--- test/mailers/previews/account_transfer_mailer_preview.rb
+++ test/mailers/previews/account_transfer_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/account_transfer_mailer
 class AccountTransferMailerPreview < ActionMailer::Preview
+  def transfer_request
+    account = Account.first
+    transfer = AccountTransfer.new(account: account, original_owner: account.owner, target_owner: 'test@example.com')
+    transfer.save
 
+    AccountTransferMailer.with(transfer: transfer).transfer_request
+  end
 end
